5aa2c50931f4007bbe3f7295913e610957b1fe6c
Mark Kelly <mark.kelly@lexisnexisrisk.com>
2019-10-06 20:51:02 -0400

    Undo chunk_size

--- xclip.c
+++ xclip.c
@@ -98,8 +98,6 @@ static struct requestor *get_requestor(Window win)
 	    errmalloc();
 	} else {
 	    requestor->context = XCLIB_XCIN_NONE;
-        // mck - large captures ...
-	    requestor->chunk_size = 20 * 1024 * 1024;
 	}
 
 	if (!requestors) {
